af801cdf2d55064edaefc5fa106426e813bb2923
Renee Mao <renee.mao217@gmail.com>
2025-02-17 12:12:53 -0500

    Update ui.ts

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,10 +1,10 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
-export const CHAT_HEADER: string = `[CHAT HEADER]`;
+export const CHAT_HEADER: string = `Lil ray of forking sunshine`;
 export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
-export const FOOTER_MESSAGE: string = `[FOOTER MESSAGE]`;
+export const FOOTER_MESSAGE: string = `use it at your own risk`;
 export const CLEAR_BUTTON_TEXT: string = `[CLEAR BUTTON TEXT]`;
 export const PAGE_TITLE: string = `[PAGE TITLE]`;
-export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
+export const PAGE_DESCRIPTION: string = `Chat with ${Lil ray of forking sunshine}, ${Renee}'s AI assistant.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
