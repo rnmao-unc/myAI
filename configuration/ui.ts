c09a929a04063c3726efa8eec25e3874e9976658
Renee Mao <renee.mao217@gmail.com>
2025-02-17 12:14:29 -0500

    Update ui.ts

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -5,6 +5,6 @@ export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
 export const FOOTER_MESSAGE: string = `use it at your own risk`;
 export const CLEAR_BUTTON_TEXT: string = `[CLEAR BUTTON TEXT]`;
 export const PAGE_TITLE: string = `[PAGE TITLE]`;
-export const PAGE_DESCRIPTION: string = `Chat with ${Lil ray of forking sunshine}, ${Renee}'s AI assistant.`;
+export const PAGE_DESCRIPTION: string = `Chat with ${sunshine}, ${Renee}'s AI assistant.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
