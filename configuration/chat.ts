8f5998c72dbf125800e9b15fe2bb808d4c383ca0
Renee Mao <renee.mao217@gmail.com>
2025-03-01 00:19:58 -0500

    Update chat.ts

--- configuration/chat.ts
+++ configuration/chat.ts
@@ -2,6 +2,6 @@ import { OWNER_NAME, AI_NAME } from "./identity";
 
 export const INITIAL_MESSAGE: string = `Hello, I'm Recipe Finder, Foodie Renee's AI assistant.`;
 export const DEFAULT_RESPONSE_MESSAGE: string = `Sorry, I'm having trouble finding your plate. Please try again later.`;
-export const WORD_CUTOFF: number = 8000; // Number of words until bot says it needs a break
+export const WORD_CUTOFF: number = 10000; // Number of words until bot says it needs a break
 export const WORD_BREAK_MESSAGE: string = `-`;
 export const HISTORY_CONTEXT_LENGTH: number = 7; // Number of messages to use for context when generating a response
