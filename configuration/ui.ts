10410deaf6050f9b20981c979d581964f4e735f2
Renee Mao <renee.mao217@gmail.com>
2025-03-01 00:00:19 -0500

    Update ui.ts

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,10 +1,10 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
 export const CHAT_HEADER: string = `Recipe Finder`;
-export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
+export const MESSAGE_PLACEHOLDER: string = `Recipe Finder is the best!`;
 export const FOOTER_MESSAGE: string = `Customized by R.Mao`;
-export const CLEAR_BUTTON_TEXT: string = `[CLEAR BUTTON TEXT]`;
-export const PAGE_TITLE: string = `[PAGE TITLE]`;
-export const PAGE_DESCRIPTION: string = `Chat with sunshine, Renee's AI assistant.`;
+export const CLEAR_BUTTON_TEXT: string = `Clear Chat`;
+export const PAGE_TITLE: string = `Recipe Finder`;
+export const PAGE_DESCRIPTION: string = `Chat with recipe finder, AI assistant to help you eat better.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
