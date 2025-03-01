f6a521933358b8436c994b540dfc7c46a8176257
Renee Mao <renee.mao217@gmail.com>
2025-02-28 23:56:05 -0500

    Change footer msg and chat name to my own

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,8 +1,8 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
-export const CHAT_HEADER: string = `sunshine`;
+export const CHAT_HEADER: string = `Recipe Finder`;
 export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
-export const FOOTER_MESSAGE: string = `use it at your own risk`;
+export const FOOTER_MESSAGE: string = `Customized by R.Mao`;
 export const CLEAR_BUTTON_TEXT: string = `[CLEAR BUTTON TEXT]`;
 export const PAGE_TITLE: string = `[PAGE TITLE]`;
 export const PAGE_DESCRIPTION: string = `Chat with sunshine, Renee's AI assistant.`;
